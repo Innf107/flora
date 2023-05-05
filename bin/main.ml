1b1aac2f16cd4cff995dc81ba26cbe431780bffe
Prophet <finn@famschneider.info>
2023-05-06 00:19:19 +0200

    Add --continue --string

--- bin/main.ml
+++ bin/main.ml
@@ -5,21 +5,23 @@ type flora_options = {
   write_env_to_file : string option;
   read_env_from_file : string option;
   write_effects_to_file : string option;
-  is_continue : string option;
+  is_continue : [ `Raw of string | `Expr of string | `No ];
 }
 
 let usage =
   {|Usage: flora [options] [file]
 
     Options
-      --help                 Show this message
-      --effects FILE         Enable effect handling. The continuation will be written to FILE
-      --continue EXPR        Continue a previously suspended continuation in FILE with argument EXPR
-      --write-env FILE       Write the final evaluation environment to FILE
-      --read-env FILE        Read the initial evaluation environment from FILE
+      --help                      Show this message
+      --effects FILE              Enable effect handling. The continuation will be written to FILE
+      --continue [--string] EXPR  Continue a previously suspended continuation in FILE with argument EXPR
+                                  If this is passed --string, it will treat the argument as a raw literal string
+                                  without needing delimiters
+      --write-env FILE            Write the final evaluation environment to FILE
+      --read-env FILE             Read the initial evaluation environment from FILE
 
-      --trace CATEGORY       Enable interpreter traces for debugging purposes.
-                               Possible values: |}
+      --trace CATEGORY            Enable interpreter traces for debugging purposes.
+                                    Possible values: |}
   ^ String.concat ", " (Trace.get_categories ())
 
 let error_usage message =
@@ -36,8 +38,10 @@ let rec parse_args options = function
   | "--effects" :: file :: rest ->
       parse_args { options with write_effects_to_file = Some file } rest
   | [ "--effects" ] -> error_usage "Option '--write-env' expects an argument"
+  | "--continue" :: "--string" :: str :: rest ->
+      parse_args { options with is_continue = `Raw str } rest
   | "--continue" :: expr :: rest ->
-      parse_args { options with is_continue = Some expr } rest
+      parse_args { options with is_continue = `Expr expr } rest
   | [ "--continue" ] -> error_usage "Option '--continue' expects an argument"
   | "--write-env" :: file :: rest ->
       parse_args { options with write_env_to_file = Some file } rest
@@ -135,7 +139,7 @@ let () =
       write_env_to_file = None;
       read_env_from_file = None;
       write_effects_to_file = None;
-      is_continue = None;
+      is_continue = `No;
     }
   in
   let options = parse_args initial_options (List.tl (Array.to_list Sys.argv)) in
@@ -151,41 +155,7 @@ let () =
   in
 
   match options.is_continue with
-  | Some argument_string -> begin
-      match options.file_to_run with
-      | None ->
-          prerr_endline
-            "--continue is only valid when running a continuation file";
-          exit 1
-      | Some file ->
-          Error.handle
-            ~handler:(fun error ->
-              prerr_endline ("ERROR: " ^ Error.pretty error);
-              exit 1)
-            begin
-              fun () ->
-                let _env, argument_value =
-                  match
-                    Driver.eval_string ~filename:None Syntax.empty_env
-                      argument_string
-                  with
-                  | Eval.Completed value -> value
-                  | Eval.Suspended (effect, _, _) ->
-                      prerr_endline
-                        ("Unhandled effect in continuation argument: '" ^ effect
-                       ^ "'");
-                      exit 1
-                in
-
-                let cont =
-                  In_channel.with_open_bin file
-                    (Serialize.deserialize (DeserializeCont EnvValue))
-                in
-                let result = Eval.continue cont argument_value in
-                handle_result options result
-            end
-    end
-  | None -> begin
+  | `No -> begin
       match options.file_to_run with
       | None ->
           if Unix.isatty Unix.stdin then begin
@@ -204,4 +174,43 @@ let () =
           In_channel.with_open_text filename (fun in_channel ->
               run_file in_channel ~filename:(Some filename) initial_env options)
         end
+    end
+  | _ -> begin
+      match options.file_to_run with
+      | None ->
+          prerr_endline
+            "--continue is only valid when running a continuation file";
+          exit 1
+      | Some file ->
+          Error.handle
+            ~handler:(fun error ->
+              prerr_endline ("ERROR: " ^ Error.pretty error);
+              exit 1)
+            begin
+              fun () ->
+                let argument_value =
+                  match options.is_continue with
+                  | `Expr expr_string -> begin
+                      match
+                        Driver.eval_string ~filename:None Syntax.empty_env
+                          expr_string
+                      with
+                      | Eval.Completed (_, value) -> value
+                      | Eval.Suspended (effect, _, _) ->
+                          prerr_endline
+                            ("Unhandled effect in continuation argument: '"
+                           ^ effect ^ "'");
+                          exit 1
+                    end
+                  | `Raw str -> String str
+                  | `No -> Util.panic __LOC__ "unreachable"
+                in
+
+                let cont =
+                  In_channel.with_open_bin file
+                    (Serialize.deserialize (DeserializeCont EnvValue))
+                in
+                let result = Eval.continue cont argument_value in
+                handle_result options result
+            end
     end
